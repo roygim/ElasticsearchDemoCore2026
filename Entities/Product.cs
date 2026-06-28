eaabaacbf92218be988896708885c418fcb5f839
Roei <roygim@gmail.com>
2026-06-28 16:27:24 +0300

    roeig

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -4,4 +4,6 @@ public class Product
     public int Id { get; set; }
     public string Name { get; set; }
     public double Price { get; set; }
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
 }
