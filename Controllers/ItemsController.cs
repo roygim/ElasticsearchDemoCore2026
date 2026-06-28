9dec0204e7e7855aaa7fa527f30c127b5d6abab4
Roei <roygim@gmail.com>
2026-06-28 10:31:26 +0300

    initial commit

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoCore2026.Controllers
+{
+    [ApiController]
+    [Route("api/items")]
+    public class ItemsController : ControllerBase
+    {
+        public ItemsController()
+        {
+        }
+            
+        [HttpGet("all")]
+        public async Task<ActionResult> GetAll()
+        {
+            var response = "ok";
+            return Ok(response);
+        }
+    }
+}
